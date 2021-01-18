e29bfc7559d667503b11c2b25f8423a9d544c495
Dillon Kearns <dillon@dillonkearns.com>
2021-01-17 21:39:27 -0800

    Expose LanguageTag module and add some docs.

--- src/LanguageTag.elm
+++ src/LanguageTag.elm
@@ -1,13 +1,52 @@
-module LanguageTag exposing (LanguageTag, Options, build, custom, fromLanguage, noOptions, toString)
+module LanguageTag exposing
+    ( LanguageTag
+    , Options, noOptions
+    , build, custom, fromLanguage, unknown
+    , toString
+    )
+
+{-| A LanguageTag represents a BCP47 value, and can be used in the `lang` attribute of HTML
+elements to tell the web page which language it's written in, including script and region information.
+
+See <https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/lang>.
+
+@docs LanguageTag
+
+@docs Options, noOptions
+
+@docs build, custom, fromLanguage, unknown
+
+@docs toString
+
+-}
 
 import Country exposing (Country)
 import Language exposing (Language)
 import Script exposing (Script)
 
 
+{-| -}
 type LanguageTag
     = LanguageTag Language { region : Maybe Country, script : Maybe Script }
     | Custom String
+
+
+{-| Unknown tags can be expressed by an empty string in the `lang` tag (see
+<https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/lang>).
+You may want to set the language as unknown rather than an incorrect specific
+language to prevent translation plugins from attempting to translate
+the page incorrectly.
+
+    import LanguageTag
+
+    LanguageTag.unknown
+        |> LanguageTag.toString
+        --> ""
+
+-}
+unknown : LanguageTag
+unknown =
+    Custom ""
 
 
 {-| -}
@@ -21,16 +60,19 @@ noOptions =
     { region = Nothing, script = Nothing }
 
 
+{-| -}
 build : Language -> { region : Maybe Country, script : Maybe Script } -> LanguageTag
 build language options =
     LanguageTag language options
 
 
+{-| -}
 fromLanguage : Language -> LanguageTag
 fromLanguage language =
     LanguageTag language noOptions
 
 
+{-| -}
 toString : LanguageTag -> String
 toString languageTag =
     case languageTag of
@@ -51,6 +93,7 @@ toString languageTag =
                 |> String.join "-"
 
 
+{-| -}
 custom : String -> LanguageTag
 custom =
     Custom
